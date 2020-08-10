f1ba549000fd84878123fde860aee2b293ef912c
unknown <Administrator@DEV-APP.dev.local>
2020-08-10 16:39:39 -0400

    React Todo App

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is a ToDo List App made using React</p>
+            
+        </React.Fragment>
+    )
+}
+
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
+// index.js serves as the entry point for <div id="root"> in index.html
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App';            //<-- main parent App component (App.js)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ReactDOM renders the App component in the "root" element in index.html
+// React.StrictMode is used for development by activating additional checks for debugging
+ReactDOM.render(<App />, document.getElementById('root'));
